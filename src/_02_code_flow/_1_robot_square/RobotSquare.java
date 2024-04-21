481316a2b4fd3bffb74742ea6cbd16e19764b46f
league <league@192.168.1.103>
2024-04-21 12:12:59 -0700

    I worked on my first receipe.

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -11,33 +11,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
 	
     public static void main(String[] args){
- 
-    	// This code makes a new Robot
-    	Robot rob = new Robot();
-
-        // PEN.  Put the robot's pen down so it can draw, Use this command:
-		 /**     rob.penDown();    **/
-
-        // SPEED. Make the robot move quickly. Use this command:
-		  /**     rob.setSpeed(<speed>);    **/
- 
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
-		  /**     int count = 0;    **/
-    	
+		int count = 0;    
+		    // This code makes a new Robot
+	    Robot Erik = new Robot();
 
+    	while ( count < 4 ) { 
+     	
+        // PEN.  Put the robot's pen down so it can draw, Use this command:
+		     Erik.penDown();    
+
+        // SPEED. Make the robot move quickly. Use this command:
+		     Erik.setSpeed(7);    
+ 
         // DRAW.  Draw a line. Use this command:
-    		  /**     rob.move(<distance>);    **/
+    		 Erik.move(-100);  
   
+    	// TURN.  Turn 90 degrees. Use this command:
+    		 Erik.turn(90);   
+        // INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
+    		 count += 1 ; 
 
-	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn(<angle>);    **/
-
-    	
-    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
-    		  /**     count += 1;    **/
-
-    	
+    	}
     	// Run the program. Did the Robot draw a line and turn?
     	 
     	
@@ -45,12 +41,8 @@ public class RobotSquare {
     	//        so it is between the curly braces like the example below. 
     	//        The code in the loop will now repeat 4 times to draw a square.
     	
-    	/**     while ( count < 4 ) { 
-    	 *            DRAW
-    	 *            TURN
-    	 *            INCREASE COUNT
-    	 *      }
-    	 **/
+    	    
+    	 
     	
     	
     	// Run the program again. Did your Robot draw a square?
